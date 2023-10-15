f74fc24cdcee0647551dee5c8ee39dfabba6783a
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-15 22:31:19 +0300

    assignment

--- data/index.js
+++ data/index.js
@@ -1,5 +1,21 @@
-
-const baseURL= 'http://localhost:3000/characters'
-fetch('baseURL')
-    .then(res => res.json())
-    .then(data => console.log(data))
+const baseURL =  'http://localhost:3000/characters';
+fetch(baseURL)
+    .then(response => {
+        if (!response.ok){
+            throw new Error('not ok')
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error =>{
+        console.error('error with fetch', error);
+    });
+document.addEventListener("DOMContentLoaded", () => {
+    const animalList = document.getElementById('animalList');
+    //const animalData = document.getElementById('animalData');
+    const fetchAnimalData = (animalId) => {
+        fetch(1)
+    }
+})
