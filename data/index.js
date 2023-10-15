b6a00fd7d006f36599e07253c78b97b1315d5ecc
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-15 23:04:27 +0300

    assignment

--- data/index.js
+++ data/index.js
@@ -1,4 +1,10 @@
 const baseURL =  'http://localhost:3000/characters';
+    fetchData();
+
+const appendAnimalList = document.getElementById('animalList');
+const animalImage =document.getElementById('placeholder-img')
+const currentVotes= document.getElementById('currentVotes');
+const voteForm = document.getElementById('inputVotes')
 fetch(baseURL)
     .then(response => {
         if (!response.ok){
@@ -15,7 +21,34 @@ fetch(baseURL)
 document.addEventListener("DOMContentLoaded", () => {
     const animalList = document.getElementById('animalList');
     //const animalData = document.getElementById('animalData');
-    const fetchAnimalData = (animalId) => {
-        fetch(1)
-    }
-})
+    const fetchAnimalData = (id) => {
+        fetch(`http://localhost:3000/characters${id}`)
+        .then(response => response.json())
+        .then(data => {
+            const { name, image, votes} = data;
+
+            const detailsHTML = `
+                <h2>${name}<h2>
+                <img src= "${image}" alt="${name} Image"
+                <p>currentVotes: ${currentVotes}</p>
+                <button id = "voteButton">Vote</button>
+            `;
+            animalData.innerHTML= detailsHTML;
+
+            const voteButton = document.getElementById('voteButton');
+            voteButton.addEventListener('click', () => {
+                alert("voted");
+            });
+         
+        })
+    };
+    animals.forEach(animal => {
+        const animalNameElement = document.createElement("li");
+        animalNameElement.textContent = animal.name;
+        animalNameElement.addEventListener("click", () => {
+            fetchAnimalData(animal.id);
+        });
+
+        animalList.appendChild(animalNameElement);
+    });
+});
