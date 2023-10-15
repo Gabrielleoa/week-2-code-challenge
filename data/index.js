18391d82102023b79fa43b665bccf00670fe4893
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2023-10-15 23:25:02 +0300

    assignment

--- data/index.js
+++ data/index.js
@@ -1,54 +1,28 @@
-const baseURL =  'http://localhost:3000/characters';
-    fetchData();
-
-const appendAnimalList = document.getElementById('animalList');
-const animalImage =document.getElementById('placeholder-img')
-const currentVotes= document.getElementById('currentVotes');
-const voteForm = document.getElementById('inputVotes')
+const baseURL =  'http://localhost:3000';
 fetch(baseURL)
-    .then(response => {
-        if (!response.ok){
-            throw new Error('not ok')
-        }
+    .then(function (response) {
+        console.log(response);
         return response.json();
     })
-    .then(data => {
+    .then(function(data){
         console.log(data);
-    })
-    .catch(error =>{
-        console.error('error with fetch', error);
+       
     });
 document.addEventListener("DOMContentLoaded", () => {
+    console.log('voted!')
     const animalList = document.getElementById('animalList');
-    //const animalData = document.getElementById('animalData');
-    const fetchAnimalData = (id) => {
-        fetch(`http://localhost:3000/characters${id}`)
-        .then(response => response.json())
-        .then(data => {
-            const { name, image, votes} = data;
-
-            const detailsHTML = `
-                <h2>${name}<h2>
-                <img src= "${image}" alt="${name} Image"
-                <p>currentVotes: ${currentVotes}</p>
-                <button id = "voteButton">Vote</button>
-            `;
-            animalData.innerHTML= detailsHTML;
-
             const voteButton = document.getElementById('voteButton');
             voteButton.addEventListener('click', () => {
                 alert("voted");
             });
          
         })
-    };
-    animals.forEach(animal => {
-        const animalNameElement = document.createElement("li");
-        animalNameElement.textContent = animal.name;
-        animalNameElement.addEventListener("click", () => {
-            fetchAnimalData(animal.id);
-        });
-
-        animalList.appendChild(animalNameElement);
+animals.forEach(animal => {
+    const animalNameElement = document.createElement("li");
+    animalNameElement.textContent = animal.name;
+    animalNameElement.addEventListener("click", () => {
+        fetchAnimalData(animal.id);
     });
+
+    animalList.appendChild(animalNameElement);
 });
